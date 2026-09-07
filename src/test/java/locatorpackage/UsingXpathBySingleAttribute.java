dcf2cc5e3442cb85cc20c82211e16110d81d4829
Pritam <ijiopritam@gmail.com>
2026-09-07 14:01:03 +0530

    xpath commit1

---
+++ src/test/java/locatorpackage/UsingXpathBySingleAttribute.java
@@ -0,0 +1,40 @@
+package locatorpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UsingXpathBySingleAttribute
+{
+    public static void main(String[] args)
+    {
+      try
+      {
+          WebDriver driver = new ChromeDriver();
+
+          driver.manage().window().maximize();
+
+          driver.get("https://chat.qspiders.com/");
+
+          Thread.sleep(2000);
+
+          driver.findElement(By.xpath("//input[@name='username']"))
+                  .sendKeys("johndoe@gmail.com");
+
+          Thread.sleep(2000);
+
+          driver.findElement(By.xpath("//input[@name='password']"))
+                  .sendKeys("Password@12345");
+
+          Thread.sleep(2000);
+
+          driver.findElement(By.xpath("//button[text()='Login']"))
+                  .click();
+
+      }
+      catch(Exception e)
+      {
+          e.printStackTrace();
+      }
+    }
+}
